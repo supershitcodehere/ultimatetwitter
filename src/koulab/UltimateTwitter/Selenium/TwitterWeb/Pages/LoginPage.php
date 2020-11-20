f990e6279d74956ac4c0ac85d75d73c4560c31ed
kouhei <admin@cse.ne.jp>
2020-11-21 00:32:24 +0900

    updated

--- src/koulab/UltimateTwitter/Selenium/TwitterWeb/Pages/LoginPage.php
+++ src/koulab/UltimateTwitter/Selenium/TwitterWeb/Pages/LoginPage.php
@@ -1,13 +1,16 @@
 <?php
 namespace koulab\UltimateTwitter\Selenium\TwitterWeb\Pages;
 
+use Facebook\WebDriver\Exception\NoSuchElementException;
+use Facebook\WebDriver\Exception\TimeoutException;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverExpectedCondition;
 use koulab\UltimateTwitter\Selenium\TwitterWeb\TwitterWeb;
+use Symfony\Component\DomCrawler\Crawler;
 
 class LoginPage extends TwitterWeb {
 
-    public function login(string $screen_name,string $password): HomeTimeLinePage{
+    public function login(string $screen_name,string $password,string $emailAddress = null,string $phoneNumber = null): HomeTimeLinePage{
         $this->getDriver()->get('https://twitter.com/login');
 
         $this->waitWithXpath("//input[@name='session[username_or_email]']",10);
@@ -30,6 +33,30 @@ class LoginPage extends TwitterWeb {
             ->findElement(WebDriverBy::xpath("//div[@data-testid='LoginForm_Login_Button']"))
             ->click()
         ;
+
+        //Check "Verify your identity" Page
+        try {
+            $this->waitWithXpath("//*[@id='login-challenge-form']", 5);
+            $type = $this->getDriver()->findElement(WebDriverBy::name('challenge_type'))->getAttribute("value");
+            //challenge_response
+            $ans = $emailAddress;
+            if($type == 'RetypePhoneNumber'){
+                $ans = $phoneNumber;
+            }
+            $this->waitWithXpath("//input[@id='challenge_response']",3);
+            $this
+                ->getDriver()
+                ->findElement(WebDriverBy::id("challenge_response"))
+                ->sendKeys($ans)
+            ;
+            $this
+                ->getDriver()
+                ->findElement(WebDriverBy::id("email_challenge_submit"))
+                ->click()
+            ;
+        }catch (NoSuchElementException | TimeoutException $e){
+
+        }
 
         return new HomeTimeLinePage($this->getDriver());
 
