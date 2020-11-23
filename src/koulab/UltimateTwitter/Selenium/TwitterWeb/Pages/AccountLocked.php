0d4b06d4ef3a784f40f4b39199c0fc8a23c37cba
kouhei <admin@cse.ne.jp>
2020-11-23 18:01:01 +0900

    updated

--- src/koulab/UltimateTwitter/Selenium/TwitterWeb/Pages/AccountLocked.php
+++ src/koulab/UltimateTwitter/Selenium/TwitterWeb/Pages/AccountLocked.php
@@ -4,6 +4,7 @@ namespace koulab\UltimateTwitter\Selenium\TwitterWeb\Pages;
 
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
 use koulab\UltimateTwitter\Selenium\TwitterWeb\TwitterWeb;
 
 class AccountLocked extends TwitterWeb{
@@ -15,39 +16,70 @@ class AccountLocked extends TwitterWeb{
     {
         parent::__construct($driver);
 
-        $this->waitWithXpath("//form[@action='/account/access']/input[@type='submit']",5);
+        $this->getDriver()->wait(5,500)->until(
+            WebDriverExpectedCondition::presenceOfElementLocated(
+                WebDriverBy::xpath("//form[@action='/account/access']/input[@type='submit']")
+            )
+        );
     }
 
     public function start(string $gRecaptchaResponse) : HomeTimeLinePage{
+        try {
+            $this
+                ->getDriver()
+                ->findElement(WebDriverBy::xpath("//form[@action='/account/access']/input[@type='submit']"))
+                ->click();
+        }catch (\Exception $e){ }
 
-        $this
-            ->getDriver()
-            ->findElement(WebDriverBy::xpath("//form[@action='/account/access']/input[@type='submit']"))
-            ->click()
+        $this->getDriver()
+            ->executeScript('var element=document.getElementById("g-recaptcha-response"); element.style.display="";')
+        ;
+        $this->getDriver()
+            ->executeScript('var cb=document.getElementById("continue_button"); cb.style.display="";')
+        ;
+        $this->getDriver()
+            ->executeScript('var vs=document.getElementById("verification_string"); vs.value="'.$gRecaptchaResponse.'";')
         ;
 
-        // captcha site key=  ??
 
-        $captcha = $this->getDriver()
+        sleep(1);
+
+        $this->getDriver()
             ->findElement(WebDriverBy::id('g-recaptcha-response'))
+            ->sendKeys(trim($gRecaptchaResponse))
         ;
-        $this
-            ->getDriver()
-            ->executeScript('arguments[0].value = "{'.$gRecaptchaResponse.'}";',[$captcha])
-        ;
+        sleep(1);
 
-        $this->waitWithXpath(WebDriverBy::cssSelector("#continue_button"),30);
+        $this->getDriver()->wait(10,500)->until(
+            WebDriverExpectedCondition::visibilityOfElementLocated(
+                WebDriverBy::id("continue_button")
+            )
+        );
+
         $this
             ->getDriver()
-            ->findElement(WebDriverBy::xpath("#continue_button"))
+            ->findElement(WebDriverBy::id("continue_button"))
             ->click()
         ;
 
-        $this->waitWithXpath("//form/input[@type='submit']",5);
+        try {
+            $this->getDriver()->wait(10, 500)->until(
+                WebDriverExpectedCondition::presenceOfElementLocated(
+                    WebDriverBy::xpath("//form/input[@type='submit']")
+                )
+            );
+
+            $this
+                ->getDriver()
+                ->findElement(WebDriverBy::xpath("//form/input[@type='submit']"))
+                ->click()
+            ;
+        }catch (\Exception $e){ }
+
+        sleep(3);
         $this
             ->getDriver()
-            ->findElement(WebDriverBy::xpath("//form/input[@type='submit']"))
-            ->click()
+            ->get('https://twitter.com/home')
         ;
 
         return new HomeTimeLinePage($this->getDriver());
